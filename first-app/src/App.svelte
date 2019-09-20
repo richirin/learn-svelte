cc95e5f74729c43f5f928089a5cf601c072f2905
richirin <richirichirin@gmail.com>
2019-09-20 21:51:44 +0700

    update

--- first-app/src/App.svelte
+++ first-app/src/App.svelte
@@ -1,9 +1,13 @@
 <script>
+	import ContactCard from './ContactCard.svelte'
 	let name = 'Richi';
 	let age = 17;
+	let job = '';
+	let desc = '';
+	let image = '';
 	$: upperCaseName = name.toUpperCase()
-	$: console.log("it works?")
-	$: if(upperCaseName === "RICHI"){
+	// $: console.log("it works?")
+	$: if(upperCaseName === 'RICHI'){
 		age +=1;
 	}
 	function incrementAge() {
@@ -11,6 +15,10 @@
 	}
 	function changeName() {
 		name = "Richi Ramadhan";
+	}
+	function nameInput() {
+		const enteredValue = event.target.value;
+		name = enteredValue;
 	}
 </script>
 
@@ -22,4 +30,15 @@
 
 <h1>Hello {upperCaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
-<button on:click="{changeName}">Change Name</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
+<input type="text" bind:value={name}>
+<input type="text" bind:value={job}>
+<input type="text" bind:value={image}>
+<textarea name="" id="" cols="30"  bind:value="{desc}"></textarea>
+
+<ContactCard 
+	userName={name} 
+	jobTitle={job} 
+	description={desc}
+	imageName={image}/>
---
+++ first-app/src/ContactCard.svelte
@@ -0,0 +1,71 @@
+<script>
+    export let 
+        userName, 
+        jobTitle, 
+        description,
+        imageName;
+</script>
+
+<style>
+    .contact-card{
+        box-shadow : 0 2px 8px rgba(0, 0, 0, 0.26);
+        max-width : 30rem;
+        border-radius : 5px;
+        margin : 1rem 0;
+    }
+
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .thumb{
+        width : 33%;
+        height : 100%;
+    }
+
+    .user-data{
+        width : 67%;
+        display : flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-left : 1rem;
+    }
+
+    h1{
+        font-size : 1.25rem;
+        font-family : "Roboto Slab", sans-serif;
+    }
+
+    h2{
+        font-size : 1rem;
+        font-weight : normal;
+        color : #5a5a5a;
+    }
+
+    .description{
+        border-top : 1px solid #ccc;
+        padding : 1rem;
+    }
+    img{
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
+
+<div class="contact-card">
+    <header>
+        <div class="thumb">
+            <img src={imageName} alt={userName}>    
+        </div>
+        <div class="user-data">
+            <h1>{userName}</h1>
+            <h2>{jobTitle}</h2>
+        </div>
+    </header>
+    <div class="description">
+        <p>{description}</p>
+    </div>
+</div>
